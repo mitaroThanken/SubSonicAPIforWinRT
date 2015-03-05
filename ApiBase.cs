f99e80d3d3a92ba1b06e8ba6953767c500a829f4
YAMAMI Taro <mitarothanken@gmail.com>
2015-03-05 14:50:30 +0900

    SubSonic API 用ライブラリの作成準備。

---
+++ ApiBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SubsonicAPI
+{
+    public class ApiBase
+    {
+        private const string PREFIX = "com.github.mitaroThanken.SubSonicAPIforWinRT_";
+        private Uri baseUri = null;
+        private string userName = string.Empty;
+    }
+}
